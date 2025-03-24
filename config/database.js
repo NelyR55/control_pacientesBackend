8fa9be0ddce814d616b6e4ea11dd6198917b0fa2
Nely Rodriguez <nelyr844@gmail.com>
2025-03-24 10:45:48 -0600

    Actualización de backend

--- config/database.js
+++ config/database.js
@@ -1,18 +1,14 @@
 const mysql = require('mysql2');
+require('dotenv').config(); // Cargar las variables de entorno
 
-const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'control_pacientes'
-});
+const connection = mysql.createConnection(process.env.DATABASE_URL);
 
 connection.connect(err => {
   if (err) {
     console.error('Database connection failed:', err.stack);
     return;
   }
-  console.log('Connected to MySQL database');
+  console.log('Connected to MySQL database in Railway 🚀');
 });
 
 module.exports = connection;
